fc95f65004a91beed3e3575f03450679997ac1fc
Manisha Ranawat <manisha@readybytes.in>
2013-05-24 13:04:10 +0530

    error removed related to JEXEC security & GPL License Notice

--- source/plugins/rb_ecommerceprocessor/stripe/processors/stripe/lib/Stripe/Account.php
+++ source/plugins/rb_ecommerceprocessor/stripe/processors/stripe/lib/Stripe/Account.php
@@ -1,4 +1,16 @@
 <?php
+/**
+* @copyright	Copyright (C) 2009 - 2012 Ready Bytes Software Labs Pvt. Ltd. All rights reserved.
+* @license		GNU/GPL, see LICENSE.php
+* @package 		Joomla.Plugin
+* @subpackage	Rb_EcommerceProcessor.Stripe
+* @contact		team@readybytes.in
+*/
+
+// no direct access
+if(!defined( '_JEXEC' )){
+	die( 'Restricted access' );
+} 
 
 class Stripe_Account extends Stripe_SingletonApiResource
 {
