c415e3a150e71d13990d585b7ce49b8d4101abe5
Gaurav Jain <gaurav@readybytes.in>
2015-03-05 13:04:14 +0530

    chore :: Corrects header information of each php file

--- source/plugins/rb_ecommerceprocessor/paypal/processors/paypal/layouts/gateway_paypal_recurring.php
+++ source/plugins/rb_ecommerceprocessor/paypal/processors/paypal/layouts/gateway_paypal_recurring.php
@@ -1,4 +1,11 @@
 <?php 
+/**
+* @copyright	Copyright (C) 2009 - 2012 Ready Bytes Software Labs Pvt. Ltd. All rights reserved.
+* @license		GNU/GPL, see LICENSE.php
+* @package 		Joomla.Plugin
+* @subpackage	Rb_EcommerceProcessor.Paypal
+* @contact		support@readybytes.in
+*/
 // no direct access
 defined( '_JEXEC' ) or die( 'Restricted access' );
  
