a5ece26abda46230bf1d64811983b72cdd59d608
Neelam <neelam@readybytes.in>
2015-07-31 18:22:24 +0530

    Task::Implements integration of Authoize.NET (AIM) payment gateway

---
+++ source/plugins/rb_ecommerceprocessor/authorizenet/authorizenet.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+* @copyright	Copyright (C) 2009 - 2015 Ready Bytes Software Labs Pvt. Ltd. All rights reserved.
+* @license		GNU/GPL, see LICENSE.php
+* @package 		Joomla.Plugin
+* @subpackage	Rb_EcommerceProcessor.Authorizenet
+* @contact		team@readybytes.in
+*/
+
+// no direct access
+if(!defined( '_JEXEC' )){
+	die( 'Restricted access' );
+}
+
+/**
+ * @author Neelam Soni
+ *
+ */
+class  plgRb_ecommerceprocessorAuthorizenet extends RB_Plugin
+{
+	function __construct(& $subject, $config = array())
+	{
+		parent::__construct($subject, $config);
+		
+		$fileName = __DIR__.'/processors/authorizenet/authorizenet.php';
+		Rb_HelperLoader::addAutoLoadFile($fileName, 'Rb_EcommerceProcessorAuthorizenet');
+		
+		$helper = Rb_EcommerceFactory::getHelper();
+		$helper->processor->push('authorizenet', array('location' => $fileName, 'class' => 'Rb_EcommerceProcessorAuthorizenet'));
+		
+		// load language file also
+		$this->loadLanguage();
+	}
+}
