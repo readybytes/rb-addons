80a9f5fe3a9d87e99294b4fd09796a638d4dede2
Neelam <neelam@readybytes.in>
2015-10-08 15:11:18 +0530

    Fix::Fixes guest checkout issue and implements validation on phone number

--- source/plugins/rb_ecommerceprocessor/payumoney/processors/payumoney/layouts/gateway_payumoney.php
+++ source/plugins/rb_ecommerceprocessor/payumoney/processors/payumoney/layouts/gateway_payumoney.php
@@ -13,46 +13,28 @@ JHtml::_('behavior.formvalidation');
 $year 		= date('Y');
 $form_data	= $displayData;
 ?>
-
 <div class="well ">
-	
 	<div class="row-fluid">
-	
+			<div class="text-center">
+				<?php
+					//@TODO:: dont use hardcoded path
+					echo Rb_Html::image('plugins/rb_ecommerceprocessor/payumoney/processors/payumoney/layouts/logo.png', 'PayUMoney'); 
+				?>	
+			</div>
+		</div>
+	<div class="row-fluid">
 		<span class="payment-errors hide"></span>
+		<br/>
    		<fieldset>
-	   		<legend><?php echo JText::_('PLG_RB_ECOMMERCEPROCESSOR_PAYUMONEY_FORM_PAYUMONEY_FIELDSET_CUSTOMER_DETAILS');?></legend>
-	        <div class="control-group">
-	        	<div class="control-label">
-		          	<label for="firstname" title="<?php echo JText::_('PLG_RB_ECOMMERCEPROCESSOR_PAYUMONEY_FORM_PAYUMONEY_FIRST_NAME_DESC');?>">
-					     <?php echo JText::_('PLG_RB_ECOMMERCEPROCESSOR_PAYUMONEY_FORM_PAYUMONEY_FIRST_NAME_LABEL')?>
-					</label>
-				</div>
-		        <div class="controls">
-		          	<input type="text" class="input-block-level" name="firstname" id="firstname" value="<?php echo $form_data['firstname'];?>" <?php if(!empty($form_data['firstname'])){ echo "readonly";}?>></input>
-		        </div> 	        
-	          	<span for="firstname" class="rb-error hide"><?php echo  JText::_('PLG_RB_ECOMMERCEPROCESSOR_PAYUMONEY_ERROR_VALIDATION_REQUIRED'); ?></span>     
-	        </div>
-	        
-			<div class="control-group">
-				<div class="control-label">
-		          <label for="email" title="<?php echo JText::_('PLG_RB_ECOMMERCEPROCESSOR_PAYUMONEY_FORM_PAYUMONEY_EMAIL_DESC');?>">
-					    <?php echo JText::_('PLG_RB_ECOMMERCEPROCESSOR_PAYUMONEY_FORM_PAYUMONEY_EMAIL_LABEL');?>
-				  </label>
-				</div>
-		        <div class="controls">
-		          		<input type="email" class="input-block-level  validate-email" name="email" id="email" value="<?php echo $form_data['email'];?>" <?php if(!empty($form_data['email'])){ echo "readonly";}?>></input>
-		        </div>
-		        <span for="email" class="rb-error hide"><?php echo  JText::_('PLG_RB_ECOMMERCEPROCESSOR_PAYUMONEY_ERROR_EMAIL_VALIDATION_REQUIRED'); ?></span>   
-	        </div>
-	        
-	        <div class="control-group" >
+	   	   <legend><?php echo JText::_('PLG_RB_ECOMMERCEPROCESSOR_PAYUMONEY_FORM_PAYUMONEY_FIELDSET_CUSTOMER_DETAILS');?></legend>
+	       <div class="control-group" >
 		          <div class="control-label">
 	            	<label for="phone" title="<?php echo JText::_('PLG_RB_ECOMMERCEPROCESSOR_PAYUMONEY_FORM_PAYUMONEY_MOBILE_DESC');?>">
 				    	<?php echo JText::_('PLG_RB_ECOMMERCEPROCESSOR_PAYUMONEY_FORM_PAYUMONEY_MOBILE_LABEL');?>
 			 	    </label>
 			 	  </div>
 		          <div class="controls">
-		          	<input type="text" class="input-block-level" name="phone" id="phone" value="<?php echo $form_data['phone'];?>" <?php if(!empty($form_data['phone'])){ echo "readonly";}?>></input>
+		          	<input type="text" class="input-block-level required validate-rb-regex-pattern generateHash" name="phone" data-validate-pattern="^(?=.*\d.*\d.*\d)[0-9+\(\)#\.\s\/ext-]+$" id="phone" value="<?php echo $form_data['phone'];?>" <?php if(!empty($form_data['phone'])){ echo "readonly";}?>></input>
 		          </div>
 		         <span for="phone" class="rb-error hide"><?php echo  JText::_('PLG_RB_ECOMMERCEPROCESSOR_PAYUMONEY_ERROR_VALIDATION_REQUIRED'); ?></span>  
 	        </div>
@@ -60,6 +42,8 @@ $form_data	= $displayData;
 	        <input type="hidden" name="key" value="<?php echo $form_data['key'];?>" />
 	        <input type="hidden" name="txnid" value="<?php echo $form_data['txnid'];?>" />
 	        <input type="hidden" name="amount" value="<?php echo $form_data['amount'];?>" />
+	        <input type="hidden" name="firstname" value="<?php echo $form_data['firstname'];?>" />
+	        <input type="hidden" name="email" value="<?php echo $form_data['email'];?>" />
 	        <input type="hidden" name="productinfo" value="<?php echo htmlspecialchars($form_data['productinfo']);?>" />
 	        <input type="hidden" name="surl" value="<?php echo $form_data['surl'];?>" />
 	        <input type="hidden" name="furl" value="<?php echo $form_data['furl'];?>" />
@@ -67,7 +51,6 @@ $form_data	= $displayData;
 	        <input type="hidden" name="hash" value="<?php echo $form_data['hash'];?>" />
 	        <input type="hidden" name="udf1" value="<?php echo $form_data['udf1'];?>" />
 	        <input type="hidden" name="service_provider" value="<?php echo $form_data['service_provider'];?>" />
-	        
  		</fieldset>
     </div>
 </div>
